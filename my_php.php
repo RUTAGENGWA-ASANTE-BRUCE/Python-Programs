aa2a389a54a91b553dd1fd0a275bc3069aeb3eec
RUTAGENGWA-ASANTE-BRUCE <96545712+RUTAGENGWA-ASANTE-BRUCE@users.noreply.github.com>
2022-05-19 15:42:33 +0200

    new python programs

---
+++ my_php.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<head>
+    <link rel="stylesheet" href="styles_1.css">
+    <script src="My_Js.js"></script>
+    <title>bruce</title>
+
+<body>
+    <p style="border: 1px solid green " id="bruce">Bruce is fantastic</p>
+    <button type="button" onclick="call()">click me</button>
+    <br>
+    <?php echo "<h1>Hello php<h1>"?>
+</body>
